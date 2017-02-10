735eda7957fefd645593811a4e8e6c0a58d7c767
helio.saraiva <buzato@hotmail.com>
2017-02-10 18:55:06 -0200

    primeiro commit

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from core.views import home, lista, novo, atualiza, deletar
+
+urlpatterns = [
+    url(r'^$', home),
+    url(r'^lista-todos/$', lista),
+    url(r'^novo/$', novo),
+    url(r'^atualizar/(?P<id>\d+)/$', atualiza, name = 'atualizar'),
+url(r'^deletar/(?P<id>\d+)/$', deletar, name = 'deletar'),
+
+
+
+]
